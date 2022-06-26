03c6d47a2d7d22d189b9f7e17b297cb9e67c7b09
lawrence graham <lawrencegraham@lawrences-iMac.local>
2022-06-26 11:19:07 -0400

    first commit

---
+++ loops.js
@@ -0,0 +1,47 @@
+//const productLabel=[]
+//const productDetails={}
+
+
+//let iterator = 1;
+//while (iterator < 100) {
+ //   productDetails["product-" + iterator] = { productName:"something"}
+
+//}
+
+//for (let i =0; i < 6; i++){
+   // let starstring ="";
+   // for(let j = 0; j < i; j++) {
+   //     starstring+= "*";
+    //    console.log(j, starstring);
+   // }
+//}
+
+//const matrix = [
+//    [1, 2, 3],
+ //   [3, 4, 5],
+//  [5, 3, 5],
+//];
+//const arr1 =[1, 2, 3];
+
+//for (let i =0; i <matrix.length; i++){
+ //   for(let j=0; j< matrix[i].length;j++) {
+ //       console.log(matrix[i][j])
+  //  }
+//}
+
+
+let shippingStatus = false;
+//after box is shipped 
+shippingStatus = true;
+let day = 0;
+while (shippingStaus === true) {
+    console.log({ day });
+    if (day === 5){
+        shippingstaus = false;
+        console.log("your product is delivered");
+    } else {
+        console.log("your product is being shipped");
+    }
+    day=+1
+    }
+}
