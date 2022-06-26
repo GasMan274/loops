bbbf3c38b5c5e3bb914f454e7bf388eb252d9e68
lawrence graham <lawrencegraham@lawrences-iMac.local>
2022-06-26 12:20:29 -0400

    added loop.js

--- loops.js
+++ loops.js
@@ -45,3 +45,16 @@ while (shippingStaus === true) {
     day=+1
     }
 }
+
+let newInventoryCarModels = ['S1', 'T1', 'S2', 'S1', 'S2', 'S1']
+let newInventoryCarTypes = ['StockCar', 'Refrigerator', 'StockCar', 'Lorrie', 'Lorrie', 'Lorrie']
+let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
+
+let i =0
+let typeS1Count =0;
+while ( i< newInventoryCarModels.length; i++) {
+    if (newInventoryCarModels[i] === "S1"){typeS1Count += 1}
+}
+i += 1;
+
+for(let i =0; i< newInventoryCarModels.length; i++ )
